920e9083cb6e82c7e4e35b982264158dea1c450f
felipe <felipesscreator@gmail.com>
2022-09-15 14:51:06 -0300

    feat: basic routes and database

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,20 @@ import express from "express";
 
 const app = express();
 
-app.get('/ads', (req, res) => {
-  return res.json([
-    {id: 1, name: 'Anuncio 1'},
-    {id: 2, name: 'Anuncio 2'},
-    {id: 3, name: 'Anuncio 3'},
-    {id: 4, name: 'Anuncio 4'}
-  ])
+app.get('/games', (req, res) => {
+  return res.json([]);
+})
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+})
+
+app.get('games/:id/ads', (req, res) => {
+  // const gameId = req.params.id;
+})
+
+app.get('ads/:id/discord', (req, res) => {
+  // const adId = req.params.id;
 })
 
 app.listen(3333);
