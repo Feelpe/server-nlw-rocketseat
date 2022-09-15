8c8d75c010cb0266c1fc6074cc2d0e46fa7b04b8
felipe <felipesscreator@gmail.com>
2022-09-15 15:58:55 -0300

    feat: ads table prisma, predatabase, get route in progress

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient({
+  log: ['query']
+});
 
-app.get('/games', (req, res) => {
-  return res.json([]);
+app.get('/games', async (req, res) => {
+  const games = await prisma.game.findMany();
+
+  return res.json(games);
 })
 
 app.post('/ads', (req, res) => {
